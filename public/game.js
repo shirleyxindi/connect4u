136c8f5a4853eec46449d3e2d9db32eae67317c5
Amandaatje <amanda.krudde@gmail.com>
2020-01-15 15:37:19 +0100

    yess

--- public/game.js
+++ public/game.js
@@ -21,3 +21,16 @@ function createBoard(){
 }
 
 createBoard();
+
+function calctime()
+{	var t=setTimeout("calctime()",1000);
+    var m = 0;
+    if(t == 59){
+        m++;
+        t = t - 59;
+    }
+    document.getElementById('time').innerHTML="Time passed " + m + ":" + t + " sec";
+    
+}
+
+calctime();
